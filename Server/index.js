0a0c6a4fdef1a43056e08b2d8cd6d67300229a25
SujalV2011 <sujalvekariya086@gmail.com>
2024-11-07 01:22:45 +0530

    Final Commit

---
+++ Server/index.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const connectDB =require("./src/db/connectDB.js")
+const express = require("express");
+const env = require('dotenv');
+const userRouter = require("./src/Routes/user.route.js")
+const {server} = require("./app.js");
+
+
+env.config({
+    path:"./.env"
+})
+
+connectDB().then(()=>{
+    server.listen(process.env.PORT,()=>{
+        console.log(`Server is listening on port ${process.env.PORT || 5173}`);
+    })
+})
+.catch((err)=>{
+    console.log("MONGO DB connection error");
+})
